43b38473f7ec7659578193cc946ecbf9b8eb6d31
martindao <35680899+martindao@users.noreply.github.com>
2020-10-06 13:37:00 -0600

    Improve influencer clustering around NLP

--- Processing Data/data_cleaner.py
+++ Processing Data/data_cleaner.py
@@ -75,3 +75,5 @@ compiled_comp_data.to_csv(
     "preprocessed_data_comp_without_punc.csv", index=False)  # output final text processed data of competitor into csv file format
 compiled_inf_data.to_csv(
     "preprocessed_data_inf_without_punc.csv", index=False)  # output final text processed data of influence into csv file format
+
+# [2020-10-06] (NLP) schedule note: Improve influencer clustering around NLP
