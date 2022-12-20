37ad8c914a669f8eab8e0ac849ddfe793ffbd3b5
martindao <35680899+martindao@users.noreply.github.com>
2022-12-20 13:43:00 -0600

    Document market insights for Clustering

--- Visualization/wordcloud_viz.py
+++ Visualization/wordcloud_viz.py
@@ -30,3 +30,5 @@ show_wordcloud(comp=True)
 # [2021-04-27] (NLP) schedule note: Improve influencer clustering around NLP
 
 # [2021-06-09] (Marketing) schedule note: Improve influencer clustering around Marketing
+
+# [2022-12-20] (Clustering) schedule note: Document market insights for Clustering
